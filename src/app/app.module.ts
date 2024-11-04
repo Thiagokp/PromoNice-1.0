3fc311d7b392847ed2634a1d56ddd08bff58ac1a
gabri <gabriel.batista.olly@gmail.com>
2024-11-04 20:26:08 -0300

    chore: corrigindo nome dos arquivos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PaginaInicialComponent } from './Pagina-Inicial/Pagina-Inicial.component';
+import { PaginaInicialComponent } from './pagina-Inicial/Pagina-Inicial.component';
 import { FormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { HeaderComponent } from './Header/Header.component'; // Importando HeaderComponent
+import { HeaderComponent } from './header/header.component'; // Importando HeaderComponent
 import { HttpClientModule } from '@angular/common/http'; // Importando HttpClientModule
 
 @NgModule({
