7c1f58f2dfe8178adc07ec3c8bc51453c6ce3559
Fernandoscmoura <fernandosc.moura20@gmail.com>
2024-11-05 10:36:24 -0300

    alteração do layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,11 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HttpClientModule } from '@angular/common/http'; // Importando HttpClientModule
-<<<<<<< HEAD
-import { registerLocaleData } from '@angular/common';
-import localePt from '@angular/common/locales/pt';
-=======
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from "./components/header/header.component";
 
->>>>>>> dfdb0f55370a8b03a5ff92a5e1ccad993989316f
 
-registerLocaleData(localePt);
+
 @NgModule({
   declarations: [
     AppComponent,
