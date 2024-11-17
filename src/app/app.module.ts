4af4ce62e45abb1273dfc01901a2d2bf06ada66f
Ieda <gabriel.batista.olly@gmail.com>
2024-11-17 20:46:57 -0300

    funcionalidades nas telas de cadastro e login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HttpClientModule } from '@angular/common/http'; // Importando HttpClientModule
 import { HomeComponent } from './components/home/home.component';
@@ -24,6 +24,7 @@ import { RouterModule } from '@angular/router';
     FontAwesomeModule,
     HeaderComponent,
     RouterModule,
+    ReactiveFormsModule,
     HttpClientModule, // Adicione o HttpClientModule aqui
   ],
   providers: [],
