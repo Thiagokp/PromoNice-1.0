eec73431fe71e5f01d5f4a2d4d1a139bf4b88cb0
gabriel-azevedo02 <gabrielribeiroazevedo02@gmail.com>
2025-04-22 23:22:03 -0300

    correção url backend azure

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://promonice4-aievus-projects.vercel.app/'
+  apiUrl: 'https://promonice-api-hcfzadffezbth8g7.brazilsouth-01.azurewebsites.net/api'
 };
